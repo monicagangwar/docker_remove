0e3e52584aa9534e69da9912aa06bf1e0ab41a82
monicagangwar <monicagangwar10@gmail.com>
2017-02-22 21:19:47 +0530

    pretty priniting output

--- docker_remove/main.py
+++ docker_remove/main.py
@@ -10,6 +10,10 @@ command["Container"]["list"] = "ps"
 command["Container"]["remove"] = "rm"
 command["Image"]["list"] = "images"
 command["Image"]["remove"] = "rmi"
+OKGREEN = "\033[0;32m"
+WARNING = "\033[0;31m"
+BOLD = "\033[1m"
+ENDC = "\033[0m"
 
 def get_options():
   parser =  argparse.ArgumentParser(description= 'Use this simple command to remove Docker container(s) or images(s) through string matching.')
@@ -36,7 +40,7 @@ def docker_running():
 
 def remove(remove_type,remove_string,hidden,force):
   docker_running()
-  print "Removing " + remove_type + "(s) containing string " + remove_string
+  print "Removing " + remove_type + "(s) containing string " + BOLD + remove_string + ENDC
 
   list_command = ["docker",command[remove_type]["list"]]
   if hidden == True:
@@ -46,13 +50,15 @@ def remove(remove_type,remove_string,hidden,force):
   delete_count = 0
   for row in remove_type_list.split("\n"):
     if row.find(remove_string) != -1:
-      code = name = None
+      code = name =  None
+      tag = ""
       if remove_type == "Container":
         code = row.split()[0]
         name = row.split()[1]
       elif remove_type == "Image":
         code = row.split()[2]
         name = row.split()[0]
+        tag  = ":" + row.split()[1]
       
       remove_command = ["docker",command[remove_type]["remove"]]
       if force == True:
@@ -62,10 +68,10 @@ def remove(remove_type,remove_string,hidden,force):
       out,err = run_cmd(remove_command)
 
       if not err.split():
-        print "Successfully removed " + remove_type + " " + name + " with id " + code
+        print OKGREEN + "Successfully removed " + remove_type + " " + name + tag + " with id " + code + ENDC
         delete_count = delete_count + 1
       else:
-        print "Unable to remove " + remove_type + " " + name + " with id " + code + " due to error : "
+        print WARNING + "Unable to remove " + remove_type + " " + name + tag + " with id " + code + ENDC
         print err
   
   print remove_type + "(s) deleted",delete_count
